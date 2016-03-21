f885106aee973839987f25871c15c3415996eb27
szykem2 <szykem2@wp.pl>
2016-03-21 21:58:21 +0100

    random Eulerian Graph

--- eulerCircuit.py
+++ eulerCircuit.py
@@ -1,22 +1,42 @@
-def EulerCircuit(graph):
-    tour = []
+import networkx as nx
 
-    current_vertex = graph[0][0]
-    tour.append(current_vertex)
+def is_eulerian(G):
+    for v,d in G.degree_iter():
+        if d % 2 != 0:
+            return False
+    if not nx.is_connected(G):
+        return False
+    return True
 
-    while len(graph) > 0:
-        print(graph, current_vertex)
-        for edge in graph:
-            if current_vertex in edge:
-                if edge[0] == current_vertex:
-                    current_vertex = edge[1]
-                else:
-                    current_vertex = edge[0]
 
-                graph.remove(edge)
-                tour.append(current_vertex)
-                break
+def EulerCircuit(G):
+    from operator import itemgetter
+    if not is_eulerian(G):
+        raise nx.NetworkXError("G is not Eulerian.")
+    g = G.__class__(G)
+    
+    v = next(g.nodes_iter())
+    degree = g.degree
+    edges = g.edges_iter
+    get_vertex = itemgetter(1)
+
+    vertex_stack = [v]
+    last_vertex = None
+    while vertex_stack:
+        current_vertex = vertex_stack[-1]
+        if degree(current_vertex) == 0:
+            if last_vertex is not None:
+                yield (last_vertex, current_vertex)
+            last_vertex = current_vertex
+            vertex_stack.pop()
         else:
-            print("Nie istnieje cykl Eulera")
-            return None
-    return tour
+            random_edge = next(edges(current_vertex))
+            vertex_stack.append(get_vertex(random_edge))
+            g.remove_edge(*random_edge)
+
+if __name__ == '__main__':
+    G = nx.gnm_random_graph(5,10)
+    t = EulerCircuit(G)
+    print (t)
+    for i in t:
+        print(i)
--- randomEulerGraph.py
+++ randomEulerGraph.py
@@ -1,2 +1,53 @@
+import numpy as np
+import random
+import time
+#zapis do pliku
+#przyjmuje macierz sasiedztwa i nazwe pliku do stworzenia 
+def WriteToFile(Matrix,path):
+	print ('zapis macierzy sasiedztwa do pliku', path,'\n')
+	pk = open(path,'w')
+	for i in range(0,len(Matrix)):
+		pk.write('[ ')
+		for j in range(0,len(Matrix)):	
+			pk.write(str(Matrix[i][j])+' ')
+		pk.write(']\n')
+	pk.close()
+
+#wszystkie stopnie wierzcholkow musza byc parzyste 
+def IfEulerian(Matrix):
+	DeegresOfNodes_1 = Matrix.sum(axis = 0)
+	DeegresOfNodes_2 = Matrix.sum(axis = 1)
+	if any(x%2 == 1 for x in DeegresOfNodes_1):
+		return False
+	if any(x%2 == 1 for x in DeegresOfNodes_2):
+		return False	
+	if (sum(DeegresOfNodes_1) == 0) and (sum(DeegresOfNodes_2) == 0):
+		return False
+	return True 
+
 def run():
-    print("running")
+    NumberOfNodes = int(input('Podaj ilosc wierzcholkow: '))
+    Matrix = np.zeros((NumberOfNodes,NumberOfNodes),dtype=np.int)
+
+    #sprawdzenie czy wylosowany graf jest eulerowski 
+    while True:
+        for i in range(NumberOfNodes):
+            for j in range(i):
+                pick = random.randint(0,1)
+                if (pick == 1): 
+                    Matrix[i][j] = 1 
+                    Matrix[j][i] = 1
+        DeegresOfNodes_1 = Matrix.sum(axis = 0)
+        DeegresOfNodes_2 = Matrix.sum(axis = 1)
+        #print ('Stopnie: ',DeegresOfNodes_1, DeegresOfNodes_2)
+        if (IfEulerian(Matrix) == True):
+            break;
+        else:
+            Matrix = np.zeros((NumberOfNodes,NumberOfNodes),dtype=np.int)
+
+    print (Matrix)
+
+    WriteToFile(Matrix,'files/euler.txt')
+
+if __name__ == '__main__':
+    run()
